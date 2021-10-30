cd6066fb0312eda49b69be34eaf357e03a342bfc
David Martínez Trigo <davidmtrigo@hotmail.es>
2021-10-30 18:15:50 +0200

    feat: user login from Angular

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -20,12 +21,22 @@ export class LoginComponent {
     password: ['123456', [Validators.required, Validators.minLength(6)]],
   });
 
-  constructor(private fb: FormBuilder, private router: Router) {}
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private authService: AuthService
+  ) {}
 
   login() {
     console.log(this.miFormulario.value);
     // console.log(this.miFormulario.valid);
 
-    this.router.navigateByUrl('/dashboard');
+    const { email, password } = this.miFormulario.value;
+
+    this.authService.login(email, password).subscribe((res) => {
+      console.log(res);
+    });
+
+    // this.router.navigateByUrl('/dashboard');
   }
 }
