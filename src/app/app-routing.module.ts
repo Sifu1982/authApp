747af59ef3f4efb5802f021d510210048abe4cc7
David Martínez Trigo <davidmtrigo@hotmail.es>
2021-10-31 12:04:29 +0100

    feat: added useHash configuration in app-routin.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,7 +21,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      // La configuración useHash, que es por defecto "false", añade "#" al inicio de la ruta, lo que hace que Angular sea más compatible con navegadores viejos o en los lugares en los que yo no pueda cambiar la URL o la forma de llegar a la página index.html
+      useHash: false,
+    }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
